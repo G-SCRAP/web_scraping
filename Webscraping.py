2c402ae1ce4734c9b7087cdd2fbb46ae8b43e9ad
Gavin Ogren <72085418+G-SCRAP@users.noreply.github.com>
2021-09-20 10:05:58 -0700

    Update Webscraping.py

--- Webscraping.py
+++ Webscraping.py
@@ -1,6 +1,12 @@
+"""
+Gavin Ogren
+Monday September, 20th
+Webscraping Evit
+"""
 import requests
 import bs4
 
+#Created Variables
 res = requests.get('https://evit.com/high_school_career_training/hs')
 soup = bs4.BeautifulSoup(res.text, "html.parser")
 
@@ -9,6 +15,7 @@ try:
 except Exception as exc:
     print('There was a problem: %s' % (exc))
 
+#Opens file and soup finds html elements
 with open('Files/Programs.txt', 'w+') as file:
     for elem in soup.select("#ctl00_ContentPlaceHolder1_ctl11_divContent > ul"):
         file.write(elem.getText())
